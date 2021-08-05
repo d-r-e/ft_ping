8e8f3f147d973af371f194eb107d90261ec8c401
David R <egoz3ntrum@gmail.com>
2021-08-05 14:50:58 +0000

    auto commit by darodrig 05-08-2021 from 9d6df12ba0aa

--- src/time.c
+++ src/time.c
@@ -2,5 +2,6 @@
 
 float timestamp(struct timeval a, struct timeval b)
 {
-    return (((float)(a.tv_usec - b.tv_usec) / 1000) + ((float)(a.tv_sec - b.tv_sec)) * 1000);
+    return (((float)(a.tv_usec - b.tv_usec) / 1000) +
+     ((float)(a.tv_sec - b.tv_sec)) * 1000);
 }
