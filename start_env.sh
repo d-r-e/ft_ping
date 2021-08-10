e7756ac2aaf1ab4640b905b739abeda819a67fff
David R <egoz3ntrum@gmail.com>
2021-08-10 14:58:45 +0200

    added docker sysctl option to block icmp

--- start_env.sh
+++ start_env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 docker build . -t ft_ping
 if [ "${1}" == "--no-icmp" ]; then
-    docker run -e MACHINE=$(uname -n | cut -d. -f1) -it ft_ping --sysctl net.ipv4.icmp_echo_ignore_all=1
+    docker run -e MACHINE=$(uname -n | cut -d. -f1) -it --sysctl net.ipv4.icmp_echo_ignore_all=1 ft_ping
 else
     docker run -e MACHINE=$(uname -n | cut -d. -f1) -it ft_ping
 fi
