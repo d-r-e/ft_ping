40179ecff285f93b1f06e124461f909e4625cf0d
David R <egoz3ntrum@gmail.com>
2021-08-10 12:56:01 +0000

    auto commit by darodrig 10-08-2021 from c1r12s6

--- start_env.sh
+++ start_env.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-
-
 docker build . -t ft_ping
-
 if [ "${1}" == "--no-icmp" ]; then
     docker run -e MACHINE=$(uname -n | cut -d. -f1) -it ft_ping --sysctl net.ipv4.icmp_echo_ignore_all=1
 else
